5558d95f3e6fa8cfe09219c4b3c6f7722143467e
JinFree <ytrqwe12@gmail.com>
2019-07-25 12:09:47 +0900

    build for tx2

--- build_for_nano.sh
+++ build_for_nano.sh
@@ -18,4 +18,4 @@ sudo apt-get install -y \
  libgstreamer-plugins-good1.0-dev \
  libgstreamer-plugins-bad1.0-dev \
  cmake libgflags-dev v4l-utils
-mv Makefile_nano Makefile && make && mkdir bin && cd bin && wget https://pjreddie.com/media/files/yolov3-tiny.weights && chmod +x yolov3-tiny_usbcam.sh
+mv Makefile_nano Makefile && make && mkdir bin && cd bin && wget https://pjreddie.com/media/files/yolov3-tiny.weights && cd .. && chmod +x yolov3-tiny_usbcam.sh
