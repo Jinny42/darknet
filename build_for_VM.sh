4b79841771fb1c6159c126e8866149ec608390f4
JinFree <ytrqwe12@gmail.com>
2019-08-16 11:31:43 +0900

    build_for_VM_and_custom_dataset

--- build_for_VM.sh
+++ build_for_VM.sh
@@ -9,5 +9,9 @@ sudo apt-get install -y \
  libgstreamer-plugins-base1.0-dev \
  libgstreamer-plugins-good1.0-dev \
  libgstreamer-plugins-bad1.0-dev \
- cmake libgflags-dev v4l-utils
+ cmake libgflags-dev v4l-utils pyqt5-dev-tools
 mv Makefile_VM Makefile && make && mkdir bin && cd bin && wget https://pjreddie.com/media/files/yolov3-tiny.weights&& wget https://pjreddie.com/media/files/yolov3.weights
+cd .. && git clone https://github.com/tzutalin/labelImg.git && \
+cd labelImg && \
+sudo pip3 install -r requirements/requirements-linux-python3.txt && \
+make qt5py3
