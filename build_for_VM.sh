11c1a5a78e432d1731bbd30866b2cd35823b4a3c
Jin Yeong Wang <ytrqwe12@gmail.com>
2019-08-17 00:17:16 +0900

    Update build_for_VM.sh

--- build_for_VM.sh
+++ build_for_VM.sh
@@ -10,7 +10,7 @@ sudo apt-get install -y \
  libgstreamer-plugins-good1.0-dev \
  libgstreamer-plugins-bad1.0-dev \
  cmake libgflags-dev v4l-utils pyqt5-dev-tools
-mv Makefile_VM Makefile && make && mkdir bin && cd bin && wget https://pjreddie.com/media/files/yolov3-tiny.weights&& wget https://pjreddie.com/media/files/yolov3.weights
+mv Makefile_VM Makefile && make && mkdir bin && cd bin && wget https://pjreddie.com/media/files/yolov3-tiny.weights&& wget https://pjreddie.com/media/files/yolov3.weights && cp yolov3.weights ../
 cd .. && git clone https://github.com/JinFree/labelImg.git && \
 cd labelImg && \
 sudo pip3 install -r requirements/requirements-linux-python3.txt && \
