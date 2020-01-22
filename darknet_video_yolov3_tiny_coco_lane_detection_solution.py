0dae8e3bd5ab062bb3fb08f9497594a518e9f581
JinFree <ytrqwe12@gmail.com>
2020-01-22 12:30:22 +0900

    lane detection with inference video

---
+++ darknet_video_yolov3_tiny_coco_lane_detection_solution.py
@@ -0,0 +1,124 @@
+from ctypes import *
+import math
+import random
+import os
+import cv2
+import numpy as np
+import time
+import darknet
+import lane_detection as ld
+
+def convertBack(x, y, w, h):
+    xmin = int(round(x - (w / 2)))
+    xmax = int(round(x + (w / 2)))
+    ymin = int(round(y - (h / 2)))
+    ymax = int(round(y + (h / 2)))
+    return xmin, ymin, xmax, ymax
+
+
+def cvDrawBoxes(detections, img):
+    for detection in detections:
+        x, y, w, h = detection[2][0],\
+            detection[2][1],\
+            detection[2][2],\
+            detection[2][3]
+        xmin, ymin, xmax, ymax = convertBack(
+            float(x), float(y), float(w), float(h))
+        pt1 = (xmin, ymin)
+        pt2 = (xmax, ymax)
+        cv2.rectangle(img, pt1, pt2, (0, 255, 0), 1)
+        cv2.putText(img,
+                    detection[0].decode() +
+                    " [" + str(round(detection[1] * 100, 2)) + "]",
+                    (pt1[0], pt1[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                    [0, 255, 0], 2)
+    return img
+
+
+netMain = None
+metaMain = None
+altNames = None
+
+
+def YOLO():
+
+    global metaMain, netMain, altNames
+    configPath = "./cfg/yolov3-tiny.cfg"
+    weightPath = "./bin/yolov3-tiny.weights"
+    metaPath = "./cfg/coco.data"
+    if not os.path.exists(configPath):
+        raise ValueError("Invalid config path `" +
+                         os.path.abspath(configPath)+"`")
+    if not os.path.exists(weightPath):
+        raise ValueError("Invalid weight path `" +
+                         os.path.abspath(weightPath)+"`")
+    if not os.path.exists(metaPath):
+        raise ValueError("Invalid data file path `" +
+                         os.path.abspath(metaPath)+"`")
+    if netMain is None:
+        netMain = darknet.load_net_custom(configPath.encode(
+            "ascii"), weightPath.encode("ascii"), 0, 1)  # batch size = 1
+    if metaMain is None:
+        metaMain = darknet.load_meta(metaPath.encode("ascii"))
+    if altNames is None:
+        try:
+            with open(metaPath) as metaFH:
+                metaContents = metaFH.read()
+                import re
+                match = re.search("names *= *(.*)$", metaContents,
+                                  re.IGNORECASE | re.MULTILINE)
+                if match:
+                    result = match.group(1)
+                else:
+                    result = None
+                try:
+                    if os.path.exists(result):
+                        with open(result) as namesFH:
+                            namesList = namesFH.read().strip().split("\n")
+                            altNames = [x.strip() for x in namesList]
+                except TypeError:
+                    pass
+        except Exception:
+            pass
+    #cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture("../OpenCV_in_Ubuntu/Data/Lane_Detection_Videos/solidWhiteRight.mp4")
+    #cap.set(3, 1280)
+    #cap.set(4, 720)
+    out = cv2.VideoWriter(
+        "output.avi", cv2.VideoWriter_fourcc(*"MJPG"), 10.0,
+        (darknet.network_width(netMain), darknet.network_height(netMain)))
+    print("Starting the YOLO loop...")
+
+    # Create an image we reuse for each detect
+    darknet_image = darknet.make_image(darknet.network_width(netMain),
+                                    darknet.network_height(netMain),3)
+    while True:
+        prev_time = time.time()
+        ret, frame_read = cap.read()
+        if (frame_read is None):
+            break
+        #비디오 받아서 차선 인식 후 차선이 그려진 이미지 획득
+        #차선이 그려진 이미지 = laneImage
+        laneImage = ld.lane_detection_and_draw(frame_read)
+        #차선이 그려진 이미지 리사이즈 = laneImageResize
+        laneImageResize = cv2.resize(laneImage,
+                                   (darknet.network_width(netMain),
+                                    darknet.network_height(netMain)),
+                                   interpolation=cv2.INTER_LINEAR)
+        frame_rgb = cv2.cvtColor(frame_read, cv2.COLOR_BGR2RGB)
+        frame_resized = cv2.resize(frame_rgb,
+                                   (darknet.network_width(netMain),
+                                    darknet.network_height(netMain)),
+                                   interpolation=cv2.INTER_LINEAR)
+
+        detections = darknet.detect_image(netMain, metaMain, frame_resized, thresh=0.25)
+        image = cvDrawBoxes(detections, laneImageResize)
+        #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        print(1/(time.time()-prev_time))
+        cv2.imshow('Demo', image)
+        cv2.waitKey(3)
+    cap.release()
+    out.release()
+
+if __name__ == "__main__":
+    YOLO()
